3430bdc8f6d2d1dfb0ce692bc65731f7f1a15609
sousamacedo18 <sousamacedo18@gmail.com>
2022-08-19 16:21:03 -0300

    criamos o endpoints

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 
 app.use("/listausuarios",(req,res,next)=>{
-    console.log("passei aqui")
+ 
       res.status(200).send({
-        mensagem:"deu certo!!!!",
+        mensagem:"aqui é a lista de usuários!!!!",
         nome:"carlos"
       })
 });
+app.use((req,res,next)=>{
+      const erro = new Error("Não encontrado!");
+      erro.status(404);
+      next(erro);
+});
+app.use((error,req,res,next)=>{
+        res.status(error.status || 500);
+        return res.json({
+            erro:{
+                 mensagem:error.message
+            }
+        })
+})
 
 module.exports = app
