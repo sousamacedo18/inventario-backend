66b7054940a62f719bfa7d1e30b4eb4dedb74f76
sousamacedo18 <sousamacedo18@gmail.com>
2022-08-23 16:37:33 -0300

    alterei rota

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 const rotaUsuarios = require('./routes/rotaUsuario');
+
 app.use("/usuario",rotaUsuarios);
+
 
 app.use((req,res,next)=>{
       const erro = new Error("Não encontrado!");
--- routes/rotaUsuario.js
+++ routes/rotaUsuario.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-
+//para consultar todos os dados
 router.get('/',(req,res,next)=>{
     res.status(200).send({
         mensagem:"aqui é a lista de usuários!!!!",
         nome:"carlos"
       })
 })
+//para consultar um determinado cadastro
 router.get('/:id',(req,res,next)=>{
     const id = req.params.id;
     res.status(200).send({
@@ -15,8 +16,18 @@ router.get('/:id',(req,res,next)=>{
         nome:"carlos"
       })
 })
+// para enviar dados para salvar no banco
 router.post('/',(req,res,next)=>{
-    
+
+          const usuario={
+            nome : req.body.nome,
+             email : req.body.email,
+             senha : req.body.senha
+          }
+          res.status(201).send({
+            mensagem:"Dados Inseridos!",
+            usuarioCriado:usuario
+          })
 })
 
 module.exports = router;
